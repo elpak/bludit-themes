7051cd4de86543cb5580fa0f710073762d8468cd
Panos Kontogiannis <pano1@mail.bg>
2020-05-23 16:29:00 +0300

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,11 @@
+<div class="site-footer">
+  <footer class="site-info">
+    <div class="inside-site-info grid-container grid-parent">
+      <div class="copyright-bar">
+        <span class="copyright">
+          <?php echo $site->footer(); ?>
+		  Powered by <a target="_blank" class="text-white" href="https://www.bludit.com">Bludit</a> &amp; <a target="_blank" class="text-white" href="https://themes.blog7.org">themes.blog7.org</a>
+        </div>
+    </div>
+  </footer>
+</div>
